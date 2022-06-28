ce8cbd959ea03511b69981bdfc87a24d7cc9765a
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-06-28 16:22:23 -0700

    Add zip

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
+use std::convert::AsRef;
 use std::env;
+use std::fs::File;
 use std::path::Path;
-use std::io::{Read, Seek};
-use calamine::{Cell, DataType, Range, Reader, Xlsx}; // Also: CellType
+use std::io::{BufReader, Read, Seek};
+use calamine::{Cell, DataType, Range, Reader, Ods}; // Also: CellType
 
-type Workbook<T> = Xlsx<T>;
+type Workbook<T> = Ods<T>;
 type Worksheet = (String, Range<DataType>);
 type Row = [DataType];
 type CellPosition = (u32, u32);
 
 fn main() {
+    std::process::exit(main_with_exit_code());
+}
+
+fn main_with_exit_code () -> i32 {
     let args: Vec<String> = env::args().collect();
-    for arg in args { do_arg(arg) }
+    for arg in args { do_path(&arg) };
+    0
 }
 
-fn do_arg(arg: String) {
-    let path = Path::new(&arg);
-    do_path(path);
-}
-
-fn do_path(path: &Path) {
-    let workbook = calamine::open_workbook(path).expect("Cannot open workbook");
+fn do_path(path: impl AsRef<Path>) {
+    do_zip(&path);
+    let workbook = calamine::open_workbook(&path).expect("Cannot open workbook");
     do_workbook(workbook);
 }
 
@@ -61,4 +64,44 @@ fn do_cell(cell: Cell<DataType>) {
 
 fn do_data(data: &DataType) {
     println!("data: {data:?}");
+}
+
+fn do_zip(path: impl AsRef<Path>) {
+    let file = File::open(&path).expect("file open");
+    let reader = BufReader::new(file);
+
+    let mut archive = zip::ZipArchive::new(reader).unwrap();
+
+    for i in 0..archive.len() {
+        let file = archive.by_index(i).unwrap();
+        let outpath = match file.enclosed_name() {
+            Some(path) => path,
+            None => {
+                println!("Entry {} has a suspicious path", file.name());
+                continue;
+            }
+        };
+
+        {
+            let comment = file.comment();
+            if !comment.is_empty() {
+                println!("Entry {} comment: {}", i, comment);
+            }
+        }
+
+        if (*file.name()).ends_with('/') {
+            println!(
+                "Entry {} is a directory with name \"{}\"",
+                i,
+                outpath.display()
+            );
+        } else {
+            println!(
+                "Entry {} is a file with name \"{}\" ({} bytes)",
+                i,
+                outpath.display(),
+                file.size()
+            );
+        }
+    }
 }
